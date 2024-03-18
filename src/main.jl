c1aa847db7e6f8fb3ee356d2877b6c69b512a2d9
Diogo Venâncio <diogo.venancio00@gmail.com>
2024-03-18 21:44:02 +0000

    Feat/p2 (#13)
    * feat: wip - p2
    
    * feat: handle global

--- src/main.jl
+++ src/main.jl
@@ -40,7 +40,53 @@ global_env = Env(Dict{Symbol,Any}(), nothing)
 # --------------------------------------------------------------------------- #
 
 
-function handle_make_function(args::Any, body::Expr, scope::Env)
+function is_primitive(expr::Expr, env::Env)
+    return false
+end
+
+
+function is_primitive(expr::Symbol, env::Env)
+    val = get_value(env, expr)  # Tries to fetch the value of the symbol from the environment
+
+    if val === nothing
+        x = getfield(Base, expr)  # Fetches the primitive function from the Base module
+        return x !== nothing
+    else
+        return false
+    end
+end
+
+
+function make_function(args::Any, body::Expr, scope::Env)
+    # 1. Creates a new function
+    func = (params...) -> begin
+
+        evaled_params = [eval_expr(param, scope) for param in params]
+
+        # 1.1. Takes the arguments and binds them to the function parameters
+        bindings = Dict{Symbol,Any}(zip(args, evaled_params))
+
+        # 1.2. Before evaluating the body, we extend the function scope with the bindings
+        for (var, value) in bindings
+            set_value!(scope, var, value)
+        end
+
+        # 1.3. Evaluates the body in the function scope
+        eval_expr(body, scope)
+    end
+
+    # 2. Return the function
+    return func
+end
+
+
+function eval(expr::Expr, env::Env)
+    eval_expr(expr, env)
+end
+set_value!(global_env, :eval, eval)
+
+
+function make_fexpr(args::Any, body::Union{Expr, Symbol}, scope::Env)
     # 1. Creates a new function
     func = (params...) -> begin
         # 1.1. Takes the arguments and binds them to the function parameters
@@ -72,11 +118,12 @@ function handle_call(expr::Expr, env::Env)
     # 1. Extracts the function expression from the environment
     func = eval_expr(func_name, env)
 
-    # 2. Evaluates the arguments before binding them to the function parameters
-    args = [eval_expr(arg, env) for arg in func_args]
+    if is_primitive(func_name, env)
+        func_args = [eval_expr(arg, env) for arg in func_args]
+    end
 
     # 3. Calls the function with the evaluated arguments and returns the result
-    func(args...)
+    func(func_args...)
 end
 
 
@@ -139,7 +186,7 @@ function handle_assignment(expr::Expr, eval_env::Env, storing_env::Env)
         fn_name = lhs.args[1]
         args = lhs.args[2:end]
         body = rhs
-        func = handle_make_function(args, body, eval_env)
+        func = make_function(args, body, eval_env)
 
         var = fn_name
         value = func
@@ -182,7 +229,7 @@ function handle_anonymous_function(expr::Expr, env::Env)
     args = isa(args, Symbol) ? [args] : args.args
 
     # 2. Creates a function 
-    func = handle_make_function(args, body, env)
+    func = make_function(args, body, env)
 
     # 3. Returns the function
     return func
@@ -192,11 +239,35 @@ end
 function handle_global(expr::Expr, env::Env)
     val = nothing
     for arg in expr.args
-        if arg.head === :(=)  # Global keywords are always assignments, but we do a 2nd check
+        if arg.head === :(=)
             val = handle_assignment(arg, env, global_env)
+        elseif arg.head === :(:=)
+            val = handle_fexpr(arg, env)
+        else
+            throw(ArgumentError("Invalid global statement"))
         end
     end
     return val
+end
+
+
+function handle_fexpr(expr::Expr, env::Env)
+    signature = expr.args[1]
+    body = expr.args[2]
+    fn_name = signature.args[1]
+    args = signature.args[2:end]
+
+    # 1. Makes sure the arguments are a vector of symbols
+    args = isa(args, Symbol) ? [args] : args
+
+    # 2. Creates a function 
+    func = make_fexpr(args, body, env)
+
+    # 3. Stores the function in the environment
+    set_value!(env, fn_name, func)
+
+    # 4. Returns the function
+    return func
 end
 
 
@@ -253,6 +324,8 @@ function eval_expr(expr::Expr, env::Env)
         handle_let(expr, env)
     elseif expr.head === :(=)
         handle_assignment(expr, env, env)
+    elseif expr.head === :(:=)
+        handle_fexpr(expr, env)
     elseif expr.head === :function
         # TODO: Implement function declaration
         # 1. Create a closure with the current environment
